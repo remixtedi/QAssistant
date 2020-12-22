998024f5026ba27db3b4a225ee037734cbba86fc
Giorgi Tediashvili <tediashvili@live.com>
2020-12-22 23:35:51 +0400

    ElementIdentifier Attribute

---
+++ QAssistant/Helpers/ElementIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QAssistant.Helpers
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class ElementIdentifier : Attribute
+    {
+        private readonly string identifier;
+
+        public ElementIdentifier(string identifier)
+        {
+            this.identifier = identifier;
+        }
+    }
+}
