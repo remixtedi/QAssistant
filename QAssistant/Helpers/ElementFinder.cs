125058b7a0a6e160dc7797d2f29cf5e6f3c6b138
Giorgi Tediashvili <tediashvili@live.com>
2020-12-22 23:36:10 +0400

    ElementFinder Using ElementIdentifier

---
+++ QAssistant/Helpers/ElementFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace QAssistant.Helpers
+{
+    public class ElementFinder<T>
+    {
+        public IWebElement Find(T page, string elementIdentifier)
+        {
+            var value = GetValue(typeof(RemoteWebElement), page, elementIdentifier);
+            return (IWebElement) value;
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(T page, string elementIdentifier)
+        {
+            var value = GetValue(typeof(ReadOnlyCollection<IWebElement>), page, elementIdentifier);
+            return (ReadOnlyCollection<IWebElement>) value;
+        }
+
+        private static object GetValue(Type type, T page, string elementIdentifier)
+        {
+            elementIdentifier = elementIdentifier.ToLower();
+            var props = page.GetType().GetProperties();
+
+            if (props.Any(propInfo =>
+                propInfo.Name.ToLower() == elementIdentifier && propInfo.GetValue(page)?.GetType() == type))
+                return props.First(propInfo =>
+                        propInfo.Name.ToLower() == elementIdentifier &&
+                        propInfo.GetValue(page, null)?.GetType() == type)
+                    .GetValue(page, null);
+            if (props.Any(propInfo => propInfo.CustomAttributes.Any(attrData =>
+                                          attrData.AttributeType == typeof(ElementIdentifier)
+                                          && attrData.ConstructorArguments.Any(arg =>
+                                              ((string) arg.Value)?.ToLower() == elementIdentifier))
+                                      && propInfo.GetValue(page, null)?.GetType() == type))
+                return props.First(propInfo => propInfo.CustomAttributes
+                        .Any(attrData => attrData.AttributeType == typeof(ElementIdentifier)
+                                         && attrData.ConstructorArguments.Any(arg =>
+                                             ((string) arg.Value)?.ToLower() == elementIdentifier)
+                                         && propInfo.GetValue(page, null)?.GetType() == type))
+                    .GetValue(page, null);
+
+            return null;
+        }
+    }
+}
---
+++ Tests/ElementFinderTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using QAssistant.Extensions;
+using QAssistant.Helpers;
+
+namespace Tests
+{
+    public class ElementFinderTests
+    {
+        private IWebDriver _driver;
+        private GooglePage _googlePage;
+        private ElementFinder<GooglePage> _elementFinder;
+
+        [SetUp]
+        public void Setup()
+        {
+            var opts = new ChromeOptions();
+            opts.AddArgument("--start-maximized");
+            _driver = new ChromeDriver(opts);
+            _googlePage = new GooglePage(_driver);
+            _elementFinder = new ElementFinder<GooglePage>();
+            _driver.Navigate().GoToUrl("https://google.com");
+        }
+
+        [Test]
+        public void TestElementIdentifierOnSuccessWithElementIdentifier()
+        {
+            Assert.True(typeof(RemoteWebElement) == _elementFinder.Find(_googlePage, "logoelement").GetType());
+        }
+
+        [Test]
+        public void TestElementIdentifierOnFailWithElementIdentifier()
+        {
+            Assert.IsNull(_elementFinder.Find(_googlePage, "InvalidIdentifier"));
+        }
+
+        [Test]
+        public void TestElementIdentifierOnSuccessWithPropertyName()
+        {
+            Assert.True(typeof(RemoteWebElement) == _elementFinder.Find(_googlePage, "Logo").GetType());
+        }
+
+        [Test]
+        public void TestElementIdentifierOnFailWithPropertyName()
+        {
+            Assert.IsNull(_elementFinder.Find(_googlePage, "InvalidName"));
+        }
+
+        [Test]
+        public void TestElementFinderFindElementsOnSuccess()
+        {
+            Assert.True(typeof(ReadOnlyCollection<IWebElement>) ==
+                        _elementFinder.FindElements(_googlePage, "links").GetType());
+        }
+
+        [Test]
+        public void TestElementFinderFindElementsOnFail()
+        {
+            Assert.IsNull(_elementFinder.FindElements(_googlePage, "InvalidIdentifier"));
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.CloseAndDispose();
+        }
+    }
+}
