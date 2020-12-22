7f33baf811c579dd5f919dbe8a974cf734926065
Giorgi Tediashvili <tediashvili@live.com>
2020-12-22 17:41:38 +0400

    RandomGenerator Added

---
+++ QAssistant/Helpers/RandomGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+namespace QAssistant.Helpers
+{
+    public class RandomGenerator
+    {
+        private const int DefaultLength = 10;
+        private const int DefaultRandomNumberMin = 0;
+        private const int DefaultRandomNumberMax = 999;
+        private readonly Random _random = new Random();
+
+
+        public string RandomSymbols(int length)
+        {
+            var builder = new StringBuilder(length);
+            for (var i = 0; i < length; i++)
+            {
+                var @char = (char) _random.Next(33, 47);
+                builder.Append(@char);
+            }
+
+            return builder.ToString();
+        }
+
+        public int RandomNumber()
+        {
+            return _random.Next(DefaultRandomNumberMin, DefaultRandomNumberMax);
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        public string RandomString(int length, bool lowerCase)
+        {
+            var builder = new StringBuilder(length);
+            var offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26;
+
+            for (var i = 0; i < length; i++)
+            {
+                var @char = (char) _random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+
+        public string RandomString(int length)
+        {
+            return RandomString(length, false);
+        }
+
+        public string RandomString(bool lowerCase)
+        {
+            return RandomString(DefaultLength, lowerCase);
+        }
+
+        public string RandomString()
+        {
+            return RandomString(DefaultLength, false);
+        }
+    }
+}
---
+++ Tests/RandomGeneratorTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using QAssistant.Helpers;
+
+namespace Tests
+{
+    internal class RandomGeneratorTests
+    {
+        private readonly RandomGenerator _generator;
+
+        public RandomGeneratorTests()
+        {
+            _generator = new RandomGenerator();
+        }
+
+        [Test]
+        public void RandomSymbols()
+        {
+            Assert.True(_generator.RandomSymbols(10).Any(char.IsSymbol));
+        }
+
+        [Test]
+        public void GenerateOnly1RandomSymbol()
+        {
+            Assert.True(_generator.RandomSymbols(1).Length == 1);
+        }
+
+        [Test]
+        public void ValueIsNotEmpty()
+        {
+            Assert.IsNotEmpty(_generator.RandomString());
+        }
+
+        [Test]
+        public void LengthIsCorrect()
+        {
+            Assert.AreEqual(30, _generator.RandomString(30).Length);
+        }
+
+        [Test]
+        public void ValueIsUppercase()
+        {
+            Assert.IsTrue(_generator.RandomString(30, false).Any(char.IsUpper));
+        }
+
+        [Test]
+        public void ValueIsLowercase()
+        {
+            Assert.IsTrue(_generator.RandomString(30, true).Any(char.IsLower));
+        }
+
+        [Test]
+        public void VRandomStringReturnsOnlyAlphabeticalCharacters()
+        {
+            Assert.IsFalse(_generator.RandomString(30).Any(char.IsNumber));
+        }
+
+        [Test]
+        public void RandomNumberToStrinsIsNumeric()
+        {
+            Assert.True(_generator.RandomNumber().ToString().Any(char.IsNumber));
+        }
+
+        [Test]
+        public void RandomNumberReturnsIntType()
+        {
+            Assert.AreEqual(_generator.RandomNumber().GetType(), typeof(int));
+        }
+
+        [Test]
+        public void RandomNumberReturnsMin0Max999()
+        {
+            var value = _generator.RandomNumber();
+            Assert.IsTrue(value >= 0 && value <= 999);
+        }
+
+        [Test]
+        public void RandomNumberReturnsMin10Max20()
+        {
+            var value = _generator.RandomNumber(10, 20);
+            Assert.IsTrue(value >= 10 && value <= 20);
+        }
+    }
+}
