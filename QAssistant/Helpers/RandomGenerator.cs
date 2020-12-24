a1e69fa020a1f810642ece90c3be8fb5b190fdbe
Giorgi Tediashvili <tediashvili@live.com>
2020-12-24 17:35:50 +0400

    Comments for RandomGenerator

--- QAssistant/Helpers/RandomGenerator.cs
+++ QAssistant/Helpers/RandomGenerator.cs
@@ -19,6 +19,11 @@ namespace QAssistant.Helpers
             '>', '=', '?', '@', '^', '&', '[', ']', '{', '}', '`', '~'
         };
 
+        /// <summary>
+        ///     Selects a random symbols from the <see cref="_symbols" /> list.
+        /// </summary>
+        /// <param name="length">Length of value</param>
+        /// <returns>Randomly selected symbols as <see cref="string" /> type value.</returns>
         public string RandomSymbols(int length)
         {
             var builder = new StringBuilder();
@@ -32,16 +37,36 @@ namespace QAssistant.Helpers
             return builder.ToString();
         }
 
+        /// <summary>
+        ///     Generates a random integer from range of <see cref="DefaultRandomNumberMin" /> and
+        ///     <see cref="DefaultRandomNumberMax" />
+        /// </summary>
+        /// <returns>Randomly generated <see cref="int" /> type value.</returns>
         public int RandomNumber()
         {
             return _random.Next(DefaultRandomNumberMin, DefaultRandomNumberMax);
         }
 
+        /// <summary>
+        ///     Generates a random integer from range of min and max.
+        /// </summary>
+        /// <param name="min">Minimum value.</param>
+        /// <param name="max">Maximum value.</param>
+        /// <returns>Randomly generated <see cref="int" /> type value using the passed range.</returns>
         public int RandomNumber(int min, int max)
         {
             return _random.Next(min, max);
         }
 
+        /// <summary>
+        ///     Selects a random letter, digit or symbol character.
+        /// </summary>
+        /// <param name="length">Length of value.</param>
+        /// <param name="lowerCase">
+        ///     <see langword="true" /> if you want to generate lower case string, otherwise
+        ///     <see langword="false" />
+        /// </param>
+        /// <returns>Randomly generated <see cref="string" /> type value.</returns>
         public string RandomString(int length, bool lowerCase)
         {
             var builder = new StringBuilder(length);
@@ -57,21 +82,47 @@ namespace QAssistant.Helpers
             return builder.ToString();
         }
 
+        /// <summary>
+        ///     Selects a random uppercase letter, digit or symbol character.
+        /// </summary>
+        /// <param name="length">Length of value.</param>
+        /// <returns>Randomly generated <see cref="string" /> type value.</returns>
         public string RandomString(int length)
         {
             return RandomString(length, false);
         }
 
+        /// <summary>
+        ///     Selects a random letter, digit or symbol character with length of <see cref="DefaultLength" />.
+        /// </summary>
+        /// <param name="lowerCase">
+        ///     <see langword="true" /> if you want to generate lower case string, otherwise
+        ///     <see langword="false" />
+        /// </param>
+        /// <returns>Randomly generated <see cref="string" /> type value.</returns>
         public string RandomString(bool lowerCase)
         {
             return RandomString(DefaultLength, lowerCase);
         }
 
+        /// <summary>
+        ///     Selects a random uppercase letter, digit or symbol character with length of <see cref="DefaultLength" />.
+        /// </summary>
+        /// <returns>Randomly generated <see cref="string" /> type value.</returns>
         public string RandomString()
         {
             return RandomString(DefaultLength, false);
         }
 
+        /// <summary>
+        ///     Selects a random letter.
+        /// </summary>
+        /// <param name="length">Length of value.</param>
+        /// <param name="lowerCase">
+        ///     <see langword="true" /> if you want to generate lower case string, otherwise
+        ///     <see langword="false" />
+        /// </param>
+        /// <returns>Randomly generated <see cref="string" /> type value of letters.</returns>
         public string RandomLetters(int length, bool lowerCase)
         {
             var builder = new StringBuilder(length);
@@ -87,21 +138,47 @@ namespace QAssistant.Helpers
             return lowerCase ? builder.ToString().ToLower() : builder.ToString();
         }
 
+        /// <summary>
+        ///     Selects a random uppercase letter.
+        /// </summary>
+        /// <param name="length">Length of value.</param>
+        /// <returns>Randomly generated <see cref="string" /> type value of letters.</returns>
         public string RandomLetters(int length)
         {
             return RandomLetters(length, false);
         }
 
+        /// <summary>
+        ///     Selects a random letter.
+        /// </summary>
+        /// <param name="lowerCase">
+        ///     <see langword="true" /> if you want to generate lower case string, otherwise
+        ///     <see langword="false" />
+        /// </param>
+        /// <returns>Randomly generated <see cref="string" /> type value of letters.</returns>
         public string RandomLetters(bool lowerCase)
         {
             return RandomLetters(DefaultLettersLength, lowerCase);
         }
 
+        /// <summary>
+        ///     Selects a random letter.
+        /// </summary>
+        /// <returns>Randomly generated <see cref="string" /> value.</returns>
         public string RandomLetter()
         {
             return RandomLetters(DefaultLetterLength, false);
         }
 
+        /// <summary>
+        ///     Selects a random letters and digits with passed length.
+        /// </summary>
+        /// <param name="length">Length of value.</param>
+        /// <param name="lowerCase">
+        ///     <see langword="true" /> if you want to generate lower case string, otherwise
+        ///     <see langword="false" />
+        /// </param>
+        /// <returns>Randomly generated <see cref="string" /> type value of letters and numbers.</returns>
         public string RandomDigitsAndLetters(int length, bool lowerCase)
         {
             var builder = new StringBuilder(length);
@@ -114,16 +191,33 @@ namespace QAssistant.Helpers
             return lowerCase ? builder.ToString().ToLower() : builder.ToString();
         }
 
+        /// <summary>
+        ///     Selects a random uppercase letters and digits with passed length.
+        /// </summary>
+        /// <param name="length">Length of value.</param>
+        /// <returns>Randomly generated <see cref="string" /> type value of uppercase letters and digits.</returns>
         public string RandomDigitsAndLetters(int length)
         {
             return RandomDigitsAndLetters(length, false);
         }
 
+        /// <summary>
+        ///     Selects a random letters and digits with length of <see cref="DefaultLength" />.
+        /// </summary>
+        /// <param name="lowerCase">
+        ///     <see langword="true" /> if you want to generate lower case string, otherwise
+        ///     <see langword="false" />
+        /// </param>
+        /// <returns>Randomly generated <see cref="string" /> type value of uppercase letters and digits.</returns>
         public string RandomDigitsAndLetters(bool lowerCase)
         {
             return RandomDigitsAndLetters(DefaultLength, lowerCase);
         }
 
+        /// <summary>
+        ///     Selects a random uppercase letters and digits with length of <see cref="DefaultLength" />.
+        /// </summary>
+        /// <returns>Randomly generated <see cref="string" /> type value of uppercase letters and digits.</returns>
         public string RandomDigitsAndLetters()
         {
             return RandomDigitsAndLetters(DefaultLength, false);
